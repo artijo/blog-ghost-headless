9e1ad56ade66b0c0a3709fc960329caacdd3eb70
tijo <81065469+artijo@users.noreply.github.com>
2024-02-05 15:12:31 +0700

    Update helper.ts

--- src/app/helper.ts
+++ src/app/helper.ts
@@ -1,11 +1,13 @@
+import { env } from "process";
+
 const url = process.env.GHOST_API_URL || "https://ghost.artijo.com/ghost/api/content/";
 
 export const getPosts = async (limit:Number = 9) => {
-  const response = await fetch(url + "posts/?key=" + process.env.GHOST_API_KEY+"&limit="+limit+"&include=tags");
+  const response = await fetch(url + "posts/?key=" + process.env.GHOST_API_KEY + "&limit=" + limit + "&include=tags");
   return response.json();
 };
 
 export const getPost = async (slug: string) => {
-  const response = await fetch(url + "posts/slug/" + slug + "/?key=" + process.env.GHOST_API_KEY+"&include=tags");
+  const response = await fetch(url + "posts/slug/" + slug + "/?key=" + process.env.GHOST_API_KEY + "&include=tags");
   return response.json();
 };
