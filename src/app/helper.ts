80b21077e4632b64ea9f10d08f626b16fbd2508d
tijo <81065469+artijo@users.noreply.github.com>
2024-02-16 09:10:57 +0700

    Update helper.ts

--- src/app/helper.ts
+++ src/app/helper.ts
@@ -3,7 +3,7 @@ const key = process.env.GHOST_API_KEY || "53b6258f2bcd0522281eab3a11";
 
 export const getPosts = async (limit:Number = 9) => {
   try{
-    const response = await fetch(url + "posts/?key=" + key + "&limit=" + limit + "&include=tags");
+    const response = await fetch(url + "posts/?key=" + key + "&limit=" + limit + "&include=tags", { next: { revalidate: 3600 } });
     return response.json();
   }catch(e){
     return console.log(e);
@@ -12,7 +12,7 @@ export const getPosts = async (limit:Number = 9) => {
 
 export const getPost = async (slug: string) => {
   try{
-    const response = await fetch(url + "posts/slug/" + slug + "/?key=" + key + "&include=tags");
+    const response = await fetch(url + "posts/slug/" + slug + "/?key=" + key + "&include=tags", { next: { revalidate: 3600 } });
     if(response.status === 404){
       return console.log("Not found");
     }
@@ -24,7 +24,7 @@ export const getPost = async (slug: string) => {
 
 export const getPostsByPage = async (page: Number = 1, limit:Number = 9) => {
   try{
-    const response = await fetch(url + "posts/?key=" + key + "&limit=" + limit + "&page=" + page + "&include=tags");
+    const response = await fetch(url + "posts/?key=" + key + "&limit=" + limit + "&page=" + page + "&include=tags", { next: { revalidate: 3600 } });
    return response.json();
   } catch(e){
     return console.log(e);
